5a591cf08584a6ba87899c3a94320066979a0586
dannyzaffalon <dannyzaffalon@hotmail.com>
2024-03-26 15:15:01 +0100

    ex07

--- c00/ex07/ft_putnbr.c
+++ c00/ex07/ft_putnbr.c
@@ -1,45 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dazaffal <dazaffal@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/26 14:29:18 by dazaffal          #+#    #+#             */
+/*   Updated: 2024/03/26 14:36:22 by dazaffal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-void ft_putchar(char c)
+void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
 
-void ft_positives(int nb)
+void	ft_positives(int nb)
 {
-	int i;
-	char array_num[10];
+	int		i;
+	char	array_num[10];
 
 	i = 9;
-	while(nb > 0 && i >= 0)
+	while (nb > 0 && i >= 0)
 	{
 		array_num[i] = nb % 10;
 		array_num[i] = array_num[i] + 48;
 		nb = nb / 10;
-		i = i - 1;	
+		i--;
 	}
 	i = i + 1;
-	while(i < 10)
-	{	
+	while (i < 10)
+	{
 		ft_putchar(array_num[i]);
 		i = i + 1;
 	}
 }
 
-void ft_putnbr(int nb)
+void	ft_putnbr(int nb)
 {
-	if (nb == -2147483648) write(1, "-2147483648", 11);
-	else if (nb < 0) 
+	if (nb == -2147483648)
+		write(1, "-2147483648", 11);
+	else if (nb < 0)
 	{
 		nb = 0 - nb;
 		ft_putchar('-');
-		ft_positives(nb); 
+		ft_positives(nb);
 	}
-	else if (nb > 0) ft_positives(nb);
-	else ft_putchar('0');
+	else if (nb > 0)
+		ft_positives(nb);
+	else
+		ft_putchar('0');
 }
 
-int main(void)
+/*int	main(void)
 {
 	ft_putnbr(-2147483648);
 	ft_putchar('\n');
@@ -51,4 +66,4 @@ int main(void)
 	ft_putchar('\n');
 	ft_putnbr(42);
 	ft_putchar('\n');
-}
+}*/
