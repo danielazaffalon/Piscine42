5d8e742fda4ed343597e3165c251cbebd08934e9
dannyzaffalon <dannyzaffalon@hotmail.com>
2024-03-30 14:11:21 +0100

    c03/ex04

--- c03/ex04/ft_strstr.c
+++ c03/ex04/ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: dazaffal <dazaffal@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 11:16:35 by dazaffal          #+#    #+#             */
-/*   Updated: 2024/03/28 17:41:18 by dazaffal         ###   ########.fr       */
+/*   Updated: 2024/03/30 14:00:00 by dazaffal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,19 @@ char	*ft_strstr(char *str, char *tofind)
 	char	*point;
 
 	point = 0;
-	if (*str == '\n' && *tofind == '\n')
+	if (*str == '\0' && *tofind == '\0')
 		point = str;
-	while (*str != '\0' && point == 0)
+	else
 	{
-		i = 0;
-		while (tofind[i] != '\0' && str[i] == tofind[i])
-			i++;
-		if (tofind[i] == '\0')
-			point = str;
-		str++;
+		while (*str != '\0' && point == 0)
+		{
+			i = 0;
+			while (tofind[i] != '\0' && str[i] == tofind[i])
+				i++;
+			if (tofind[i] == '\0')
+				point = str;
+			str++;
+		}
 	}
 	return (point);
 }
