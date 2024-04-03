61e35bb1d8b545ebc576b35c62f8dda7f649b8ac
dannyzaffalon <dannyzaffalon@hotmail.com>
2024-04-03 11:28:00 +0200

    c04/ex04

--- c04/ex04/ft_putnbr_base.c
+++ c04/ex04/ft_putnbr_base.c
@@ -6,7 +6,7 @@
 /*   By: dazaffal <dazaffal@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 16:19:34 by dazaffal          #+#    #+#             */
-/*   Updated: 2024/04/02 19:30:24 by dazaffal         ###   ########.fr       */
+/*   Updated: 2024/04/02 23:43:51 by dazaffal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,23 +17,52 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-void	ft_positives(int nb, char *base)
+int	base_valid(char *base)
 {
-	int		i;
-	int		base_size;
-	char	array_num[32];
+	int	base_size;
+	int	bi;
+	int	rta;
 
 	base_size = 0;
-	while (base[base_size] != '\0')
+	rta = 0;
+	while (base[base_size] != '\0' && rta == 0)
 	{
+		bi = base_size + 1;
+		if (base[base_size] == '+' || base[base_size] == '-')
+			rta = -1;
+		while (base[bi] != '\0' && rta == 0)
+		{
+			if (base[base_size] == base[bi])
+				rta = -1;
+			bi++;
+		}
 		base_size++;
 	}
+	if (base_size < 2 || rta == -1)
+		rta = -1;
+	else
+		rta = base_size;
+	return (rta);
+}
+
+void	ft_positives(unsigned int nb, char *base)
+{
+	int		i;
+	char	array_num[32];
+	int		base_size;
+
 	i = 31;
-	while (nb > 0)
+	base_size = base_valid(base);
+	if (base_size == -1)
+		array_num[i] = '\0';
+	else
 	{
-		array_num[i] = base[nb % base_size];
-		nb = nb / base_size;
-		i--;
+		while (nb > 0)
+		{
+			array_num[i] = base[nb % base_size];
+			nb = nb / base_size;
+			i--;
+		}
 	}
 	i = i + 1;
 	while (i < 32)
@@ -45,13 +74,13 @@ void	ft_positives(int nb, char *base)
 
 void	ft_putnbr_base(int nbr, char *base)
 {
-	if (nbr == -2147483648)
-		write(1, "-2147483648", 11);
-	else if (nbr < 0)
+	unsigned int	nbr_ext;
+
+	if (nbr < 0)
 	{
-		nbr = 0 - nbr;
+		nbr_ext = 0 - nbr;
 		ft_putchar('-');
-		ft_positives(nbr, base);
+		ft_positives(nbr_ext, base);
 	}
 	else if (nbr > 0)
 		ft_positives(nbr, base);
@@ -59,7 +88,7 @@ void	ft_putnbr_base(int nbr, char *base)
 		ft_putchar('0');
 }
 
-int	main(void)
+/*int	main(void)
 {
 	ft_putnbr_base(-2147483648,"0123456789");
 	ft_putchar('\n');
@@ -89,13 +118,14 @@ int	main(void)
     write(1, "\n0:", 3);
     ft_putnbr_base(0, "0123456789abcdef");
     write(1, "\nINT_MAX:", 9);
-    ft_putnbr_base(INT_MAX, "0123456789abcdef");
+    ft_putnbr_base(2147483647, "0123456789abcdef");
     write(1, "\nINT_MAX:", 9);
-    ft_putnbr_base(INT_MAX, "ZYXWVUTSRQPONMLKJIHGFEDCBAzyxwvutsrqponmlkjihgfedcba9876543210");
+    ft_putnbr_base(2147483647, "ZYXWVUTSRQPONMLKJIHGFEDCBA
+zyxwvutsrqponmlkjihgfedcba9876543210");
     write(1, "\nINT_MIN:", 9);
-    ft_putnbr_base(INT_MIN, "0123456789abcdef");
+    ft_putnbr_base(-2147483648, "0123456789abcdef");
     write(1, "\n-2143247366 : ", 15);
-    ft_putnbr_base(INT_MIN + 4236282, "'~");
+    ft_putnbr_base(-2147483648 + 4236282, "'~");
     write(1, "\n-1:", 4);
     ft_putnbr_base(-1, "0123456789abcdef");
     write(1, "\n:", 2);
@@ -106,4 +136,5 @@ int	main(void)
     ft_putnbr_base(42, "+-0123456789abcdef");
     write(1, "\n:", 2);
     ft_putnbr_base(42, "\v0123456789abcdef");
-}
+	write(1, "\n", 2);
+}*/
