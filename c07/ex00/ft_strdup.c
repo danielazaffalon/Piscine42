af3e86b3a66da3efd932024a469072de391a3adb
dannyzaffalon <dannyzaffalon@hotmail.com>
2024-04-05 14:30:36 +0200

    c07/ex00

--- c07/ex00/ft_strdup.c
+++ c07/ex00/ft_strdup.c
@@ -6,13 +6,12 @@
 /*   By: dazaffal <dazaffal@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 12:29:45 by dazaffal          #+#    #+#             */
-/*   Updated: 2024/04/05 13:10:48 by dazaffal         ###   ########.fr       */
+/*   Updated: 2024/04/05 14:29:24 by dazaffal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-#include <stdio.h>
-
+//#include <stdio.h>
 int	ft_strlen(char *str)
 {
 	int	count;
@@ -27,21 +26,21 @@ char	*ft_strdup(char *src)
 {
 	char	*rta;
 	int		len;
+	int		i;
 
 	len =  ft_strlen(src) + 1;
 	rta = (char*)malloc(len);
-	free(rta);
-	while (*src != '\0')
+	i = 0;
+	while (i < len)
 	{
-		*rta = *src;
-		printf("%c ",*rta);
-		src++;
-		rta++;
+		rta[i] = src[i];
+		//printf("%c ",*rta);
+		i++;
 	}
 	return (rta);
 }
 
-int main(int ac, char **av)
+/*int main(int ac, char **av)
 {
 	int	i;
 	char *rta;
@@ -53,4 +52,4 @@ int main(int ac, char **av)
 		printf("%s\n", rta);
 		i++;
 	}
-}
+}*/
