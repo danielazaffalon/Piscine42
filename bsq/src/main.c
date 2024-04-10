9e16b04a46d55a59c7f6ae6a96fad0ce80ad64b4
dannyzaffalon <dannyzaffalon@hotmail.com>
2024-04-10 17:33:55 +0200

    src

--- bsq/src/main.c
+++ bsq/src/main.c
@@ -14,17 +14,27 @@
 #include "map.h"
 #include "print.h"
 
-int	create_file()
+int	create_file(struct s_map *map)
 {
 	int		file;
 	char	c;
+	int		errors;
 
 	file = open("new.ox", O_RDWR | O_CREAT);
 	while (read(0, &c, 1) > 0)
 		write(file, &c, 1);
-	write(file, "\0", 1);
 	close(file);
-	return(1);
+	if(file >= 0)
+		errors = ft_get_map("new.ox", &map);
+	if (!errors)
+	{
+		iterate_square(&map);
+		write_square("new.ox", &map);
+		print_square("new.ox");
+		free(map->graph);
+	}
+	else
+		write(1, "Map error\n", 10);
 }
 
 int	main(int ac, char **av)
@@ -33,28 +43,24 @@ int	main(int ac, char **av)
 	int				errors;
 	int				i;
 
-	i = 0;
-	while (i < ac)
+	if (ac == 1)
+		errors = create_file(&map);
+	else
 	{
-		if (ac == 1)
+		i = 1;
+		while (i < ac)
 		{
-			create_file();
-			errors = ft_get_map("new.ox", &map);
-		}
-		else
-		{
-			i = 1;
 			errors = ft_get_map(av[i], &map);
+			if (!errors)
+			{
+				iterate_square(&map);
+				write_square(av[i], &map);
+				print_square(av[i]);
+				free(map.graph);
+			}
+			else
+				write(1, "Map error\n", 10);
+			i++;
 		}
-		if (!errors)
-		{
-			iterate_square(&map);
-			write_square(av[i], &map);
-			print_square(av[i]);
-			free(map.graph);
-		}
-		else
-			write(1, "Map error\n", 10);
-		i++;
 	}
 }
